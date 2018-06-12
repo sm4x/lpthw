b51bc4e2d08008f2a6bbd46e89173eac31878558
sm4x <max.schattauer@gmail.com>
2018-06-12 10:05:28 +0200

    Add files via upload

---
+++ ex39.py
@@ -0,0 +1,16 @@
+# Lists
+things = ['a', 'b', 'c', 'd']
+print(things[1])
+
+things[1] = 'z'
+print(things[1])
+
+print(things)
+
+# Dicts
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 +2 }
+print(stuff['name'])
+print(stuff['age'])
+print(stuff['height'])
+stuff['city'] = 'SF'
+print(stuff['city'])
